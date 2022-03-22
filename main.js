20c4689cb523e83b0b5f140177ad5c4cf399f676
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-22 16:14:11 +0100

    aggiunta array di oggetti

--- main.js
+++ main.js
@@ -1,1 +1,75 @@
-console.log('JS OK!')
+console.log('JS OK!');
+
+// JSnack 1 ---------------
+//Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine.
+
+
+//creazione oggetto con zucchine
+const zucchine = [
+
+  {
+    varietà: "Zucchina nera",
+    peso: 0.7,
+    lunghezza: 20,
+
+  },
+  {
+    varietà: "Zucchino romanesco",
+    peso: 0.9,
+    lunghezza: 30,
+
+  },
+  {
+    varietà: "Zucchino fiorentino",
+    peso: 1.2,
+    lunghezza: 25,
+
+  },
+  {
+    varietà: "Zucchina napoletana",
+    peso: 0.5,
+    lunghezza: 33,
+
+  },
+  {
+    varietà: "Zucchine tonde",
+    peso: 1,
+    lunghezza: 24,
+
+  },
+  {
+    varietà: "Zucchina pâtisson",
+    peso: 0.6,
+    lunghezza: 23,
+
+  },
+  {
+    varietà: "Zucchina Crookneck",
+    peso: 1.5,
+    lunghezza: 28,
+
+  },
+  {
+    varietà: "Zucchino rugoso friulano",
+    peso: 1.3,
+    lunghezza: 19,
+
+  },
+  {
+    varietà: "Zucchine trombetta",
+    peso: 0.9,
+    lunghezza: 31,
+
+  },
+  {
+    varietà: "Zucchino giallo",
+    peso: 0.8,
+    lunghezza: 22,
+
+  },
+
+
+]
+
+
+//
